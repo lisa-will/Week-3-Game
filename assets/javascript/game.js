efe5d6134e8237702e37844678cb36ca828ea7fa
Lisa Williams <williams.lisarenee@gmail.com>
2016-10-05 22:56:36 -0400

    added purrrfect images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,18 @@
+// THIS IS JS - :SCREAM: EMOJI 
+
 var wins = 0;
-var wordOptions = ["your guess is as good as mine", "a little bird told me", "heard it through the grapevine", "piece of cake", "once in a blue moon", "method to my madness", "it takes two to tango", "at the drop of a hat"];
+
+var wordOptions = [ 
+		"YOUR GUESS IS AS GOOD AS MINE",
+		"A LITTLE BIRD TOLD ME",
+		"HEARD IT THROUGH THE GRAPEVINE",
+		"PIECE OF CAKE",
+		"ONCE IN A BLUE MOON",
+		"METHOD TO MY MADNESS", 
+		"IT TAKES TWO TO TANGO", 
+		"AT THE DROP OF A HAT"
+		];
+
 var randomWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
 var numGuess = 6;
 var incorrectLetters = [];
@@ -20,22 +33,29 @@ var currentWord = [];
 	console.log(randomWord);
 	console.log(currentWord);
 
-	//THIS CODE BELOW CREATES VISUAL CHANGE: REMOVING COMMAS AFTER SPACE ((FROM THIS: "__," TO THIS: "__" ))//
+	// THIS CODE BELOW CREATES VISUAL CHANGE: REMOVING COMMAS AFTER SPACE --> FROM THIS: "__," TO THIS: "__" !!! 
 	
 	var currentWordHTML = currentWord.join(" ");
 	
 	document.querySelector("#currentWord").innerHTML = currentWordHTML;
 	
-	// THIS CODE SHOWS THE NUMBER OF GUESS REMAINING //
+	// THIS CODE SHOWS THE NUMBER OF GUESS REMAINING. 
 
 	document.querySelector("#guessesRemaining").innerHTML = numGuess;
 
-	// STARTS THE GAME ONCE USER PRESSES KEY //
+	// STARTS THE GAME ONCE USER PRESSES KEY CHANGING DEFAULT IMAGE TO GAME IMAGE 2, AND SO ON... 
 
 	document.onkeyup= function(event) {
-		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-		// IF USER PICKS A CORRECT LETTER //
+		if (numGuess == 6) {
+			document.querySelector(".imageDiv").innerHTML = '<img id="smiley" src="assets/images/smiley/2.png">'
+		}
+
+		
+		var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+
+		// IF USER PICKS A CORRECT LETTER: 
+
 		if (randomWord.indexOf(userGuess) >= 0) {
 			console.log("Correct");
 			for (var i = 0; i < randomWord.length; i++) {
@@ -47,23 +67,62 @@ var currentWord = [];
 			document.querySelector("#currentWord").innerHTML = currentWordHTML;
 		}
 
-			// IF USER PICKS LETTER ALREADY USED //
+		// IF USER PICKS LETTER ALREADY USED: 
+
 		else if (incorrectLetters.indexOf(userGuess) >= 0){
 			console.log("Already Guessed");
 		}
-			// IF USER PICKS A WRONG LETTER //
-		else{
+
+		// IF USER PICKS A WRONG LETTER: 
+
+		else {
 			numGuess --; 
 			incorrectLetters.push(userGuess);
 			
 			document.querySelector("#guessesRemaining").innerHTML = numGuess;		
 
-			incorrectLetters.push(userGuess);
-
 			incorrectLettersHTML = incorrectLetters.join(" ");
-			document.querySelector("#incorrectLetters").innerHTML = incorrectLettersHTML;
+			document.querySelector("#alreadyGuessed").innerHTML = incorrectLettersHTML;
 
+			if (numGuess == 5) {
+			document.querySelector(".imageDiv").innerHTML = '<img id="cat" src="assets/images/purrrfect/2.png">'
+			}
 
+			else if (numGuess == 4) {
+				document.querySelector(".imageDiv").innerHTML = '<img id="cat" src="assets/images/purrrfect/3.png">'
+			}
+
+			else if (numGuess == 3) {
+				document.querySelector(".imageDiv").innerHTML = '<img id="cat" src="assets/images/purrrfect/4.png">'
+			}
+
+			else if (numGuess == 2) {
+				document.querySelector(".imageDiv").innerHTML = '<img id="cat" src="assets/images/purrrfect/5.png">'
+			}
+
+			else if (numGuess == 1) {
+				document.querySelector(".imageDiv").innerHTML = '<img id="cat" src="assets/images/purrrfect/6.png">'
+			}
+
+			else if (numGuess == 0) {
+				document.querySelector(".imageDiv").innerHTML = '<img id="cat" src="assets/images/purrrfect/7.png">'
+			}
+
+			//else if (numGuess == -1) {
+				//document.querySelector(".imageDiv").innerHTML = '<img id="smiley" src="assets/images/smiley/9.png">'
+			//}
+
+	// GAME USER LOST -- SO LET'S PLAY AGAIN!!! 
+
+			function playAgain() {
+		    var x;
+		    if (confirm("PLAY AGAIN?") == true) {
+		        x = "You pressed OK!";
+		    } else {
+		        x = "You pressed Cancel!";
+		    }
+		    document.getElementById("demo").innerHTML = x;
+		}
 		
 			console.log("Wrong");
 			console.log(incorrectLetters);
